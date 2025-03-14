3115b54baa7465e11f98819d5211e7498b510f67
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-03-14 19:55:10 +0530

    week 4 assignment 1

--- Week4/index.js
+++ Week4/index.js
@@ -1,24 +1,33 @@
 const fs = require('fs');
-const { Command } = require('commander');
-const program = new Command();
+const { Command } = require("commander");
+ const program = new Command();
 
-program
-  .name('counter')
-  .description('CLI to do file based tasks')
-  .version('0.8.0');
+ program 
+  .name('Word-Counter')
+  .description('CLI based tool to count things in a file')
+  .version('1.0.0');
 
-program.command('count')
-  .description('Count the number of lines in a file')
-  .argument('<file>', 'file to count')
-  .action((file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const lines = data.split('\n').length;
-        console.log(`There are ${lines} lines in ${file}`);
+program.command('count-words')
+  .description('counts the number of words in a file')
+  .argument('<file>', 'file to count from')
+  .action(function(file){
+    fs.readFile(file,'utf-8',function(err,data){
+      if(err){
+        console.log("please enter the correct file name");
+        console.log(err.message);
       }
-    });
-  });
+      else{
+        let count = 0;
+        for(let i =0;i<data.length;i++){
+          if(data[i] === ' '){
+            count++;
+          }
+        }
+        console.log(`There are ${count+1} Words in the file given`);
+      }
+    })
 
-program.parse(); 
+  }
+)
+
+program.parse();
