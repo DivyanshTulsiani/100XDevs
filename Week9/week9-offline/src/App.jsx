0f97bdd52764c95d6ba53a2457b34edc8847dbb7
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-07-09 15:55:01 +0530

    added props and comp

--- Week9/week9-offline/src/App.jsx
+++ Week9/week9-offline/src/App.jsx
@@ -8,8 +8,12 @@ function App() {
 
   return (
    <div style={{backgroundColor: "#95a5a6",height: "100vh"}}>
-    <PostComponent/>
-    <PostComponent/>
+
+    
+    <PostComponent name="100xdevs" subtitle="23,668 followers" time="21m" description="Want to know how to learn to get 6000$ in bounties"/>
+    <br/>
+    <PostComponent name="raman" subtitle="23,000 followers"/>
+    <br/>
     <PostComponent/>
    </div>
   )
@@ -18,7 +22,17 @@ function App() {
 //a lot of devs can develop/use it 
 //define a state of ur application
 
-function PostComponent(){
+
+//props 
+//what are props 
+//props are nothing but read only properties shared between components
+//a partent component can send data to a child component
+// <component key = value/>
+//jha componet use krr rhe ho vha se directly value bhjena ke liye
+//issi ki madad se to humare componts variable honge jisse hum
+//component ko reuse krr paenge with diff values
+
+function PostComponent(props){
   
   return <div style={{background:"white",width:350,borderRadius:20,padding:20,margin: "0 auto"}}>
     <div style={{ display: "flex"}}>
@@ -31,22 +45,34 @@ function PostComponent(){
 
       <div style={{marginLeft: 10}}>
         <div>
-          100xdevs
+          {props.name}
         </div>
         <div>
-          23,388 followers
+          {props.subtitle}
         </div>
-        <div>
-          12m
-        </div>
+
+
+        {(props.time !== undefined) ? <div style={{display: "flex"}}>
+          <div>
+            {props.time}
+          </div>
+          <img src='https://media.istockphoto.com/id/1031786258/vector/watch.jpg?s=612x612&w=0&k=20&c=U_7Euy34YQwrg2zgJw_VutYLUjDHYyKUr483e-w29RE=' 
+          style={{height:10,width:10,padding:5}}/>
+        </div> : <div></div> } 
+        
       </div>
     </div>
 
     <div style={{marginTop:10}}>
-      What to know how to win big Check out these folks won 6000$ in bounties
+      {props.description}
     </div>
     </div>
   
+}
+
+//hw
+function ProfileCard(){
+
 }
 
 
